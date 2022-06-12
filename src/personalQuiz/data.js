115e92a1aaa8ebd387d8697d27f7c287fb277272
Gabriel Dias <gabriel.dias.fern@gmail.com>
2022-06-12 17:43:57 -0300

    + Add more 20 pt-br questions (BETA)

--- src/personalQuiz/data.js
+++ src/personalQuiz/data.js
@@ -84,7 +84,7 @@ const questions = [
     type: "multiple",
   },
   {
-    category: "Química",
+    category: "Ciências",
     correct_answer: "118",
     difficulty: "hard",
     incorrect_answers: [
@@ -107,6 +107,266 @@ const questions = [
     ],
     question:
       "Quais os países que têm a maior e a menor expectativa de vida do mundo?",
+    type: "multiple",
+  },
+  {
+    category: "Inglês",
+    correct_answer: "Lenda",
+    difficulty: "medium",
+    incorrect_answers: [
+      "Legenda",
+      "Conto",
+      "Legendário",
+    ],
+    question:
+      "O que a palavra legend significa em português?",
+    type: "multiple",
+  },
+  {
+    category: "Esportes",
+    correct_answer: "7",
+    difficulty: "easy",
+    incorrect_answers: [
+      "6",
+      "8",
+      "9",
+    ],
+    question:
+      "Qual o número mínimo de jogadores numa partida de futebol?",
+    type: "multiple",
+  },
+  {
+    category: "Artes",
+    correct_answer: "Pablo Picasso",
+    difficulty: "medium",
+    incorrect_answers: [
+      "Diego Rivera",
+      "Tarsila do Amaral",
+      "Salvador Dalí",
+    ],
+    question:
+      "Quem pintou “Guernica”?",
+    type: "multiple",
+  },
+  {
+    category: "Ciências",
+    correct_answer: "8 minutos",
+    difficulty: "hard",
+    incorrect_answers: [
+      "12 minutos",
+      "8 segundos",
+      "12 segundos",
+    ],
+    question:
+      "Quanto tempo a luz do Sol demora para chegar à Terra?",
+    type: "multiple",
+  },
+  {
+    category: "História",
+    correct_answer: "Argentina",
+    difficulty: "hard",
+    incorrect_answers: [
+      "Cubana",
+      "Paraguaia",
+      "Peruana",
+    ],
+    question:
+      "Qual a nacionalidade de Che Guevara?",
+    type: "multiple",
+  },
+  {
+    category: "Esportes",
+    correct_answer: "2,43m e 2,24m",
+    difficulty: "hard",
+    incorrect_answers: [
+      "2,4m para ambos",
+      "2,5m e 2m",
+      "2,45m e 2,15m",
+    ],
+    question:
+      "Qual a altura da rede de vôlei nos jogos masculino e feminino?",
+    type: "multiple",
+  },
+  {
+    category: "História",
+    correct_answer: "Paleolítico",
+    difficulty: "easy",
+    incorrect_answers: [
+      "Neolítico",
+      "Período da Pedra Polida",
+      "Idade dos Metais",
+    ],
+    question:
+      "Em que período da pré-história o fogo foi descoberto?",
+    type: "multiple",
+  },
+  {
+    category: "Língua Portuguesa",
+    correct_answer: "Substantivo, verbo e preposição.",
+    difficulty: "hard",
+    incorrect_answers: [
+      "Vogais, semivogais e consoantes",
+      "Artigo, verbo transitivo e verbo intransitivo",
+      "Fonologia, Morfologia e Sintaxe",
+    ],
+    question:
+      "Qual das alternativas abaixo apenas contêm classes de palavras?",
+    type: "multiple",
+  },
+  {
+    category: "Geografia",
+    correct_answer: "Pico da Neblina",
+    difficulty: "easy",
+    incorrect_answers: [
+      "Pico Paraná",
+      "Monte Roraima",
+      "Pico Maior de Friburgo",
+    ],
+    question:
+      "Qual a montanha mais alta do Brasil?",
+    type: "multiple",
+  },
+  {
+    category: "Ciências",
+    correct_answer: "299 792 458 m/s",
+    difficulty: "hard",
+    incorrect_answers: [
+      "300 000 000 m/s",
+      "199 792 458 m/s",
+      "30 000 000 m/s",
+    ],
+    question:
+      "Qual a velocidade da luz?",
+    type: "multiple",
+  },
+  {
+    category: "Geografia",
+    correct_answer: "Macau",
+    difficulty: "medium",
+    incorrect_answers: [
+      "Moçambique",
+      "Portugal",
+      "Filipinas",
+    ],
+    question:
+      "Em qual local da Ásia o português é língua oficial?",
+    type: "multiple",
+  },
+  {
+    category: "Entretenimento",
+    correct_answer: "Maquiavel",
+    difficulty: "easy",
+    incorrect_answers: [
+      "Antoine de Saint-Exupéry",
+      "Montesquieu",
+      "Rousseau",
+    ],
+    question:
+      "Quem é o autor de “O Príncipe”?",
+    type: "multiple",
+  },
+  {
+    category: "Língua Portuguesa",
+    correct_answer: "Eu caibo",
+    difficulty: "easy",
+    incorrect_answers: [
+      "Eu cabo",
+      "Eu coube",
+      "Eu caberei",
+    ],
+    question:
+      "Como é a conjugação do verbo caber na 1.ª pessoa do singular do presente do indicativo?",
+    type: "multiple",
+  },
+  {
+    category: "Ciências",
+    correct_answer: "Gonorreia, clamídia e sífilis.",
+    difficulty: "medium",
+    incorrect_answers: [
+      "Aids, tricomoníase e ebola",
+      "Botulismo, cistite e gonorreia",
+      "Hepatite B, febre tifoide e hanseníase",
+    ],
+    question:
+      "Quais destas doenças são sexualmente transmissíveis?",
+    type: "multiple",
+  },
+  {
+    category: "Geografia",
+    correct_answer: "Rússia",
+    difficulty: "easy",
+    incorrect_answers: [
+      "Filipinas",
+      "Marrocos",
+      "Groenlândia",
+    ],
+    question:
+      "Qual destes países é transcontinental?",
+    type: "multiple",
+  },
+  {
+    category: "Lígua Portuguesa",
+    correct_answer: "É um homem mal",
+    difficulty: "madium",
+    incorrect_answers: [
+      "Mais uma vez, portou-se mal",
+      "Esse é o mal de todos",
+      "Mal falou nele, o fulano apareceu",
+    ],
+    question:
+      "Em qual das orações abaixo a palavra foi empregada incorretamente?",
+    type: "multiple",
+  },
+  {
+    category: "Entretenimento",
+    correct_answer: "Antônio Francisco Lisboa",
+    difficulty: "hard",
+    incorrect_answers: [
+      "Alexandrino Francisco Lisboa",
+      "Manuel Francisco Lisboa",
+      "Alex Francisco Lisboa",
+    ],
+    question:
+      "Qual era o nome de Aleijadinho?",
+    type: "multiple",
+  },
+  {
+    category: "Hitória",
+    correct_answer: "Zeus e Hades",
+    difficulty: "medium",
+    incorrect_answers: [
+      "Ares e Hermes",
+      "Cronos e Apolo",
+      "Zeus e Ares",
+    ],
+    question:
+      " Júpiter e Plutão são os correlatos romanos de quais deuses gregos?",
+    type: "multiple",
+  },
+  {
+    category: "Ciências",
+    correct_answer: "Elefante africano",
+    difficulty: "easy",
+    incorrect_answers: [
+      "Baleia Azul",
+      "Dinossauro",
+      "Girafa",
+    ],
+    question:
+      "Qual o maior animal terrestre?",
+    type: "multiple",
+  },
+  {
+    category: "Hitória",
+    correct_answer: "Margaret Thatcher",
+    difficulty: "hard",
+    incorrect_answers: [
+      "Angela Merkel",
+      "Hillary Clinton",
+      "Christine Lagarde",
+    ],
+    question:
+      "Que líder mundial ficou conhecida como “Dama de Ferro”?",
     type: "multiple",
   },
 ];
@@ -206,4 +466,6 @@ const getPersonalQuestions = (amount) => {
   return returnQuestions;
 };
 
-export default getPersonalQuestions;
+console.log(questions.length);
+
+// export default getPersonalQuestions;
